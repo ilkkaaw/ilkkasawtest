1456d4a40042c3a63be28b79ba1c54ede718433e
ilkka.aw <ilkka.pekkala@awacademyconsultant.fi>
2021-07-05 14:24:06 +0300

    Made the person class.

--- git_pro.py
+++ git_pro.py
@@ -1,2 +1,15 @@
 def hello():
     return "Hello World!"
+
+class Person():
+
+    def __init__(self, name="Jose", age=20):
+        self.name = name
+        self.age = age
+
+    def getname(self):
+        return self.name
+    def getage(self):
+        return self.age
+    def __str__(self):
+        return f"My name is {self.name} and age is {self.age}"
