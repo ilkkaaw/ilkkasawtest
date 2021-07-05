4defb4815280b5e826dccb5cbb07f8c72964ee78
ilkka.aw <ilkka.pekkala@awacademyconsultant.fi>
2021-07-05 14:28:33 +0300

    Took the things from awgit

--- git_pro.py
+++ git_pro.py
@@ -1,15 +1,35 @@
 def hello():
     return "Hello World!"
 
-class Person():
+class Person:
+    def __init__(self, city, address, phone_number):
+        self._city = city
+        self._address = address
+        self._phone_number = phone_number
 
-    def __init__(self, name="Jose", age=20):
-        self.name = name
-        self.age = age
+    @property
+    def city(self, _default = None):
+        return self._city
 
-    def getname(self):
-        return self.name
-    def getage(self):
-        return self.age
+    @city.setter
+    def city(self, value):
+        self._city = value
+
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, value):
+        self._address = value
+
+    @property
+    def phone_number(self, _default=None):
+        return self._phone_number
+
+    @phone_number.setter
+    def phone_number(self, value):
+        self._phone_number = value
+
     def __str__(self):
-        return f"My name is {self.name} and age is {self.age}"
+        return f'Address: {self._address}\nCity: {self._city}\nPhone: {self._phone_number}'
